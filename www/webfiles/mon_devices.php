fdbf7dcac6da9790f35d9d7bfc9da22b771b5cf2
Douglas E. Warner <silfreed@silfreed.net>
2002-11-17 05:37:17 +0000

    global variable updates

--- www/webfiles/mon_devices.php
+++ www/webfiles/mon_devices.php
@@ -14,139 +14,122 @@
 ########################################################
 
 require_once("../include/config.php");
-require_once("/var/www/netmrg/lib/stat.php");
-require_once(netmrg_root() . "lib/format.php");
-require_once(netmrg_root() . "lib/auth.php");
-require_once(netmrg_root() . "lib/processing.php");
 check_auth(1);
 
 if ((!isset($_REQUEST["action"])) || ($_REQUEST["action"] == "doedit") || ($_REQUEST["action"] == "dodelete") || ($_REQUEST["action"] == "doadd") || ($_REQUEST["action"] =="doaddtogrp")) {
 
-if (!empty($_REQUEST["action"]) && ($_REQUEST["action"] == "doedit" || $_REQUEST["action"] == "doadd")) {
-check_auth(2);
-if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "doedit") {
-	if ($dev_id == 0) {
-		$db_cmd = "INSERT INTO";
-		$db_end = "";
+	if (!empty($_REQUEST["action"]) && ($_REQUEST["action"] == "doedit" || $_REQUEST["action"] == "doadd")) {
+		check_auth(2);
+		if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "doedit") {
+			if ($_REQUEST["dev_id"] == 0) {
+				$db_cmd = "INSERT INTO";
+				$db_end = "";
+			} else {
+				$db_cmd = "UPDATE";
+				$db_end = "WHERE id=$dev_id";
+			} // end if dev_id = 0 or not
+			if (!isset($snmp_recache)) { $snmp_recache = 0; }
+			if (!isset($disabled)) { $disabled = 0; }
+			if (!isset($snmp_check_ifnumber)) { $snmp_check_ifnumber = 0; }
+	        if (!isset($snmp_enabled)) { $snmp_enabled = 0; }
+			do_update("$db_cmd mon_devices SET 
+				name='{$_REQUEST['dev_name']}',
+				ip='{$_REQUEST['dev_ip']}',
+				snmp_read_community='{$_REQUEST['snmp_read_community']}', 
+				dev_type='{$_REQUEST['dev_type']}', 
+				snmp_recache='{$_REQUEST['snmp_recache']}', 
+				disabled='{$_REQUEST['disabled']}', 
+				snmp_check_ifnumber='{$_REQUEST['snmp_check_ifnumber']}',
+				snmp_enabled='{$_REQUEST['snmp_enabled']}' 
+				$db_end");
+
+			if ($_REQUEST["dev_id"] == 0) {
+				do_update("INSERT INTO dev_parents SET grp_id={$_REQUEST['grp_id']}, dev_id=" . mysql_insert_id());
+			} // end if dev+id = 0
+		} # done editing
+	} // end if we editing
+
+	if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "doaddtogrp") {
+		check_auth(2);
+		do_update("INSERT INTO dev_parents SET grp_id=$grp_id, dev_id=$dev_id");
+	} // end if we're adding to a group
+
+	if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "dodelete") {
+		check_auth(2);
+		delete_device($_REQUEST["dev_id"]);
+	} # done deleting
+
+	# Display a list
+	if (isset($_REQUEST["grp_id"])) {
+		$group_results = do_query("SELECT * FROM mon_groups WHERE id={$_REQUEST['grp_id']}");
+		$group_array = mysql_fetch_array($group_results);
+
+		$title = "Monitored Devices in Group '" . $group_array["name"] . "'";
+
+		$custom_add_link = "{$_SERVER['PHP_SELF']}?action=add&grp_id={$_REQUEST['grp_id']}";
+
 	} else {
-		$db_cmd = "UPDATE";
-		$db_end = "WHERE id=$dev_id";
-	}
-	if (!isset($snmp_recache)) { $snmp_recache = 0; }
-	if (!isset($disabled)) { $disabled = 0; }
-	if (!isset($snmp_check_ifnumber)) { $snmp_check_ifnumber = 0; }
-        if (!isset($snmp_enabled)) { $snmp_enabled = 0; }
-	do_update("$db_cmd mon_devices SET 
-	                name=\"$dev_name\",
-                        ip=\"$dev_ip\",
-                        snmp_read_community=\"$snmp_read_community\", 
-			dev_type=$dev_type, 
-			snmp_recache=$snmp_recache, 
-			disabled=$disabled, 
-			snmp_check_ifnumber=$snmp_check_ifnumber,
-			snmp_enabled=$snmp_enabled
-			$db_end");
+		$title = "Monitored Devices";
 
-	if ($dev_id == 0)
-	{
-		do_update("INSERT INTO dev_parents SET grp_id=$grp_id, dev_id=" . mysql_insert_id());
-	}
+	} // end if we have a group id
+	begin_page();
+	js_confirm_dialog("del", "Are you sure you want to delete device ", " and all associated items?", "{$_SERVER['PHP_SELF']}?action=dodelete&grp_id={$_REQUEST['grp_id']}&dev_id=");
+	make_display_table($title,
+	   "Name", "{$_SERVER['PHP_SELF']}?orderby=name",
+	   "SNMP Options","");
 
-} # done editing
-}
+	if (!isset($orderby)) { $orderby = "name"; };
 
-if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "doaddtogrp")
-{
-	check_auth(2);
-	do_update("INSERT INTO dev_parents SET grp_id=$grp_id, dev_id=$dev_id");
-}
+	if (!(isset($_REQUEST["grp_id"]))) {
+		$dev_results = do_query("
+			SELECT mon_devices.name AS name, mon_devices.ip, mon_devices.id
+			FROM mon_devices
+			ORDER BY $orderby");
 
-if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "dodelete")
-{
-	check_auth(2);
-	delete_device($dev_id);
-} # done deleting
+	} else {
+		$dev_results = do_query("
+			SELECT mon_devices.name AS name, mon_devices.ip, mon_devices.id 
+			FROM dev_parents
+			LEFT JOIN mon_devices ON dev_parents.dev_id=mon_devices.id 
+			WHERE grp_id={$_REQUEST['grp_id']}
+			ORDER BY $orderby");
 
+	} // end if we havea group id or not
 
-# Display a list
+	$dev_total = mysql_num_rows($dev_results);
 
-if (isset($grp_id)) {
+	# For each device
+	for ($dev_count = 1; $dev_count <= $dev_total; ++$dev_count) {
+		$dev_row = mysql_fetch_array($dev_results);
+		$dev_id  = $dev_row["id"];
 
-$group_results = do_query("SELECT * FROM mon_groups WHERE id=$grp_id");
-$group_array = mysql_fetch_array($group_results);
-
-$title = "Monitored Devices in Group '" . $group_array["name"] . "'";
-
-$custom_add_link = "{$_SERVER['PHP_SELF']}?action=add&grp_id=$grp_id";
-
-} else {
-
-$title = "Monitored Devices";
-
-}
-begin_page();
-js_confirm_dialog("del", "Are you sure you want to delete device ", " and all associated items?", "{$_SERVER['PHP_SELF']}?action=dodelete&grp_id={$_REQUEST['grp_id']}&dev_id=");
-make_display_table($title,
-				   "Name", "{$_SERVER['PHP_SELF']}?orderby=name",
-				   "SNMP Options","");
-
-if (!isset($orderby)) { $orderby = "name"; };
-
-if (!(isset($_REQUEST["grp_id"]))) {
-
-$dev_results = do_query("
-SELECT mon_devices.name AS name, mon_devices.ip, mon_devices.id
-FROM mon_devices
-ORDER BY $orderby");
-
-} else {
-
-$dev_results = do_query("
-SELECT mon_devices.name AS name, mon_devices.ip, mon_devices.id 
-FROM dev_parents
-LEFT JOIN mon_devices ON dev_parents.dev_id=mon_devices.id 
-WHERE grp_id={$_REQUEST['grp_id']}
-ORDER BY $orderby");
-
-}
-
-$dev_total = mysql_num_rows($dev_results);
-
-for ($dev_count = 1; $dev_count <= $dev_total; ++$dev_count) {
-# For each device
-
-$dev_row = mysql_fetch_array($dev_results);
-$dev_id  = $dev_row["id"];
-
-make_display_item(	$dev_row["name"],"./sub_devices.php?dev_id=$dev_id",
+		make_display_item(	$dev_row["name"],"./sub_devices.php?dev_id=$dev_id",
 			formatted_link("View Interface Cache", "snmp_cache_view.php?dev_id=$dev_id") . "&nbsp;" .
 			formatted_link("Recache Interfaces", "recache.php?dev_id=$dev_id") . "&nbsp;" .
 			formatted_link("Recache Disks", "recache.php?dev_id=$dev_id&type=disk"), "",
 			formatted_link("Edit", "{$_SERVER['PHP_SELF']}?action=edit&dev_id=$dev_id&grp_id={$_REQUEST["grp_id"]}") . "&nbsp;" .
 			formatted_link("Delete", "javascript:del('" . $dev_row["name"] . "', '" . $dev_row["id"] . "')"), "");
 
-} # end devices
+	} # end devices
 
 ?>
 </table>
 <?
 } # End if no action
 
-if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "add")
-{
+if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "add") {
 	check_auth(2);
 	begin_page();
 
 	echo("<big><b>
-	<a href='{$_SERVER['PHP_SELF']}?grp_id={$_REQUEST['grp_id']}&action=addnew'>Create a new device</a><br><br>
-	<a href='{$_SERVER['PHP_SELF']}?grp_id={$_REQUEST['grp_id']}&action=addtogrp>Add an existing device to this group</a>
-	</b></big>");
+		<a href='{$_SERVER['PHP_SELF']}?grp_id={$_REQUEST['grp_id']}&action=addnew'>Create a new device</a><br><br>
+		<a href='{$_SERVER['PHP_SELF']}?grp_id={$_REQUEST['grp_id']}&action=addtogrp>Add an existing device to this group</a>
+		</b></big>");
 
 	end_page();
-}
+} // end if add
 
-if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "addtogrp")
-{
+if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "addtogrp") {
 
 	check_auth(2);
 	begin_page();
@@ -157,43 +140,66 @@ if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "addtogrp")
 	make_edit_submit_button();
 	make_edit_end();
 	end_page();
-}
+} // end if add to group
 
 if (!empty($_REQUEST["action"]) && ($_REQUEST["action"] == "edit" || $_REQUEST["action"] == "addnew")) {
 	# Display editing screen
 	check_auth(2);
 	begin_page();
-	if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "addnew") 
-	{ 
+	if ($_REQUEST["action"] == "addnew") { 
 		$dev_id = 0; 
-	} 
+	} else {
+		$dev_id = $_REQUEST["dev_id"];
+	} // end if device id
 
-	$dev_results = do_query("SELECT * FROM mon_devices WHERE id={$_REQUEST['dev_id']}");
+	$dev_select = "SELECT * FROM mon_devices WHERE id=$dev_id";
+	echo $dev_select;
+	$dev_results = do_query($dev_select);
 	$dev_row = mysql_fetch_array($dev_results);
 	$dev_name = $dev_row["name"];
 	$dev_ip = $dev_row["ip"];
-	if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "addnew")
-	{
+	if ($_REQUEST["action"] == "addnew") {
 		$dev_row["check_if_number"] = 1;
 	}
-	make_edit_table("Edit Device");
-	make_edit_group("General");
-	make_edit_text("Name:","dev_name","25","100",$dev_name);
-	make_edit_text("IP or Host Name:","dev_ip","25","100",$dev_ip);
-	make_edit_select_from_table("Device Type:","dev_type","mon_device_types",$dev_row["dev_type"]);
-	make_edit_checkbox("Disabled (do not monitor this device)","disabled",$dev_row["disabled"]);
-	make_edit_group("SNMP");
-	make_edit_checkbox("This device uses SNMP", "snmp_enabled", $dev_row["snmp_enabled"]);
-	make_edit_text("SNMP Read Community:","snmp_read_community",50,200,$dev_row["snmp_read_community"]);
-	make_edit_checkbox("Do not cache interface mappings","snmp_recache",$dev_row["snmp_recache"]);
-	make_edit_checkbox("Clear interface cache when interface count changes","snmp_check_ifnumber",$dev_row["snmp_check_ifnumber"]);
-	make_edit_hidden("dev_id", $_REQUEST["dev_id"]);
-	make_edit_hidden("action","doedit");
-	make_edit_hidden("grp_id",$_REQUEST["grp_id"]);
-	make_edit_submit_button();
-	make_edit_end();
 
-} # End editing screen
+	if ($_REQUEST["action"] == "addnew") {
+		make_edit_table("Add Device");
+		make_edit_group("General");
+		make_edit_text("Name:","dev_name","25","100","");
+		make_edit_text("IP or Host Name:","dev_ip","25","100","");
+		make_edit_select_from_table("Device Type:","dev_type","mon_device_types","");
+		make_edit_checkbox("Disabled (do not monitor this device)","disabled","");
+		make_edit_group("SNMP");
+		make_edit_checkbox("This device uses SNMP", "snmp_enabled", "");
+		make_edit_text("SNMP Read Community:","snmp_read_community",50,200,"");
+		make_edit_checkbox("Do not cache interface mappings","snmp_recache","");
+		make_edit_checkbox("Clear interface cache when interface count changes","snmp_check_ifnumber","");
+		make_edit_hidden("dev_id", $dev_id);
+		make_edit_hidden("action","doedit");
+		make_edit_hidden("grp_id",$_REQUEST["grp_id"]);
+		make_edit_submit_button();
+		make_edit_end();
+
+	} else {
+		make_edit_table("Edit Device");
+		make_edit_group("General");
+		make_edit_text("Name:","dev_name","25","100",$dev_name);
+		make_edit_text("IP or Host Name:","dev_ip","25","100",$dev_ip);
+		make_edit_select_from_table("Device Type:","dev_type","mon_device_types",$dev_row["dev_type"]);
+		make_edit_checkbox("Disabled (do not monitor this device)","disabled",$dev_row["disabled"]);
+		make_edit_group("SNMP");
+		make_edit_checkbox("This device uses SNMP", "snmp_enabled", $dev_row["snmp_enabled"]);
+		make_edit_text("SNMP Read Community:","snmp_read_community",50,200,$dev_row["snmp_read_community"]);
+		make_edit_checkbox("Do not cache interface mappings","snmp_recache",$dev_row["snmp_recache"]);
+		make_edit_checkbox("Clear interface cache when interface count changes","snmp_check_ifnumber",$dev_row["snmp_check_ifnumber"]);
+		make_edit_hidden("dev_id", $_REQUEST["dev_id"]);
+		make_edit_hidden("action","doedit");
+		make_edit_hidden("grp_id",$_REQUEST["grp_id"]);
+		make_edit_submit_button();
+		make_edit_end();
+	} // end if addnew or edit
+
+} // end if edit
 
 end_page();
 
--- www/webfiles/recache.php
+++ www/webfiles/recache.php
@@ -1,10 +1,7 @@
 <? 
-require_once("/var/www/netmrg/lib/stat.php");
-require_once(netmrg_root() . "lib/format.php");
-require_once(netmrg_root() . "lib/snmp_caching.php");
-require_once(netmrg_root() . "lib/database.php");
+require_once("../include/config.php");
 
-$handle = do_query("SELECT * FROM mon_devices WHERE id=$dev_id");
+$handle = do_query("SELECT * FROM mon_devices WHERE id={$_REQUEST['dev_id']}");
 $row = mysql_fetch_array($handle);
 begin_page(2);
 echo "<PRE>";
