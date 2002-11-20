2c7e50eebf81e2918d9d8307525d85d2eb12704b
Douglas E. Warner <silfreed@silfreed.net>
2002-11-20 03:28:19 +0000

    making drop-downs and such use sessions

--- www/webfiles/last_status.php
+++ www/webfiles/last_status.php
@@ -15,9 +15,33 @@
 
 require_once("../include/config.php");
 check_auth(1);
-$G_tree = get_array_from_cookie("G_tree");
-$D_tree = get_array_from_cookie("D_tree");
-$M_tree = get_array_from_cookie("M_tree");
+//$G_tree = get_array_from_cookie("G_tree");
+//$D_tree = get_array_from_cookie("D_tree");
+//$M_tree = get_array_from_cookie("M_tree");
+if (!empty($_SESSION["netmrgsess"]["G_tree"]))
+{
+	$G_tree = $_SESSION["netmrgsess"]["G_tree"];
+}
+else
+{
+	$G_tree = "";
+} // end if G tree
+if (!empty($_SESSION["netmrgsess"]["D_tree"]))
+{
+	$D_tree = $_SESSION["netmrgsess"]["D_tree"];
+}
+else
+{
+	$D_tree = "";
+} // end if D tree
+if (!empty($_SESSION["netmrgsess"]["M_tree"]))
+{
+	$M_tree = $_SESSION["netmrgsess"]["M_tree"];
+}
+else
+{
+	$M_tree = "";
+} // end if M tree
 
 if (isset($_REQUEST["expand"]))
 {
@@ -39,9 +63,15 @@ if (isset($_REQUEST["expand"]))
 
 }
 
-store_array_in_cookie("G_tree",$G_tree);
-store_array_in_cookie("D_tree",$D_tree);
-store_array_in_cookie("M_tree",$M_tree);
+//store_array_in_cookie("G_tree",$G_tree);
+//store_array_in_cookie("D_tree",$D_tree);
+//store_array_in_cookie("M_tree",$M_tree);
+if (!empty($G_tree))
+	$_SESSION["netmrgsess"]["G_tree"] = $G_tree;
+if (!empty($D_tree))
+	$_SESSION["netmrgsess"]["D_tree"] = $D_tree;
+if (!empty($M_tree))
+	$_SESSION["netmrgsess"]["M_tree"] = $M_tree;
 
 refresh_tag();
 begin_page();
@@ -74,7 +104,7 @@ function draw_group($grp_id, $depth = 0)
 
 	$grp_results = do_query("SELECT * FROM mon_groups WHERE parent_id=$grp_id ORDER BY name");
 	$grp_total = mysql_num_rows($grp_results);
-	# For each group
+	// For each group
 	for ($grp_count = 1; $grp_count <= $grp_total; ++$grp_count)
 	{
 
@@ -88,7 +118,7 @@ function draw_group($grp_id, $depth = 0)
 		else
 		{
 			$img = get_image_by_name("hide");
-		} # end if G tree
+		} // end if G tree
 
 		make_display_item("<img border=0 height=1 width=" . ($depth * 8) . "><img src=\"" . $img . "\" border=\"0\"> " . $grp_row["name"], $_SERVER["PHP_SELF"] . "?expand=$grp_id&type=G","[<a href=\"./view.php?pos_id_type=0&pos_id=$grp_id\">View</a>]","","","","","",get_img_tag_from_status(get_group_status($grp_id)),"");
 
@@ -102,7 +132,7 @@ function draw_group($grp_id, $depth = 0)
 				WHERE grp_id=$grp_id
 				ORDER BY name");
         	$dev_total = mysql_num_rows($dev_results);
-			# For each device
+			// For each device
 			for ($dev_count = 1; $dev_count <= $dev_total; ++$dev_count)
 			{
 
@@ -116,7 +146,7 @@ function draw_group($grp_id, $depth = 0)
 				else
 				{
 					$img = get_image_by_name("hide");
-				} # end if D tree
+				} // end if D tree
 
 				make_display_item("","","<img src=\"" . $img . "\" border=\"0\"> " . $dev_row["name"], $_SERVER["PHP_SELF"] . "?expand=$device_id&type=D","[<a href=\"./view.php?pos_id_type=1&pos_id=$device_id\">View</a>]","","","",get_img_tag_from_status(get_device_status($device_id)),"");
 				if (isset($D_tree[$device_id]) && $D_tree[$device_id] == 1)
@@ -129,7 +159,7 @@ function draw_group($grp_id, $depth = 0)
 				        LEFT JOIN mon_devices ON mon_monitors.device_id=mon_devices.id
 				        WHERE mon_monitors.device_id=$device_id");
 					$mon_total = mysql_num_rows($mon_results);
-					# For each monitor, do test
+					// For each monitor, do test
 					for ($mon_count = 1; $mon_count <= $mon_total; ++$mon_count)
 					{
 						$mon_row = mysql_fetch_array($mon_results);
@@ -142,7 +172,7 @@ function draw_group($grp_id, $depth = 0)
 						else
 						{
 							$img = get_image_by_name("hide");
-						} # end if M tree
+						} // end if M tree
 
 						make_display_item("","","","","<img src=\"" . $img . "\" border=\"0\"> " . get_short_monitor_name($mon_row["id"]), $_SERVER["PHP_SELF"] . "?expand=$mon_id&type=M","","",get_img_tag_from_status(get_monitor_status($mon_id)) . " [Graph]","./enclose_graph.php?type=mon&id=$mon_id");
 						if ($M_tree[$mon_id] == 1)
@@ -150,7 +180,7 @@ function draw_group($grp_id, $depth = 0)
 	       					$event_results = do_query("SELECT * FROM mon_events WHERE monitors_id=$mon_id");
 							$event_total = mysql_num_rows($event_results);
 
-							# For each event
+							// For each event
 							for ($event_count = 1; $event_count <= $event_total; ++$event_count)
 							{
 								$event_row = mysql_fetch_array($event_results);
@@ -164,16 +194,16 @@ function draw_group($grp_id, $depth = 0)
 								else
 								{
 									$img = ("<img src=\"" . get_image_by_name($color . "_led_off") . "\" border=\"0\">");
-								} # end if last status
+								} // end if last status
 								make_display_item("","","","","","",$event_row["display_name"],"",$img,"");
-							} # end event for
-						} # end monitor for
-					} # end monitor expand if
-				} # end device for
-			} # end device expand if
-		} # end group for
-	} # end group expand if
-} # end draw_group()
+							} // end event for
+						} // end monitor for
+					} // end monitor expand if
+				} // end device for
+			} // end device expand if
+		} // end group for
+	} // end group expand if
+} // end draw_group()
 ?></table><?
 
 end_page();
