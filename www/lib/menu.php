4bb94269fc5f8344a13596356efe67506c963fbd
Douglas E. Warner <silfreed@silfreed.net>
2003-01-12 04:14:53 +0000

    no more database driven menu menu now is an array w/ permission levels required to view certain links

--- www/lib/menu.php
+++ www/lib/menu.php
@@ -17,35 +17,71 @@
 function display_menu()
 {
 
-	$menu_q = do_query("SELECT * FROM menu ORDER BY groupid,priority");
+	$menu = array(
+		"Monitoring" => array(
+			array("name" => "Groups", "link" => "mon_groups.php", "descr" => "", "authLevelRequired" => 1),
+			array("name" => "Device Types", "link" => "mon_device_types.php", "descr" => "", "authLevelRequired" => 2),
+			array("name" => "Notifications", "link" => "mon_notify.php", "descr" => "", "authLevelRequired" => 2)
+		),
+		"Reporting" => array(
+			array("name" => "Device Tree", "link" => "last_status.php", "descr" => "", "authLevelRequired" => 1),
+			array("name" => "Event Log", "link" => "event_log.php", "descr" => "Display a list of the most recent events.", "authLevelRequired" => 1)
+		),
+		"Graphing" => array(
+			array("name" => "Custom Graphs", "link" => "custom_graphs.php", "descr" => "", "authLevelRequired" => 1)
+		),
+		"Tests" => array(
+			array("name" => "Scripts", "link" => "tests_script.php", "descr" => "External Programs", "authLevelRequired" => 2),
+			array("name" => "SNMP", "link" => "tests_snmp.php", "descr" => "SNMP Queries", "authLevelRequired" => 2),
+			array("name" => "SQL", "link" => "tests_sql.php", "descr" => "Database Queries", "authLevelRequired" => 2)
+		),
+		"Admin" => array(
+			array("name" => "Users", "link" => "users.php", "descr" => "User Management", "authLevelRequired" => 3),
+			array("name" => "Logout", "link" => "logout.php", "descr" => "End your NetMRG Session.", "authLevelRequired" => 0)
+		),
+		"Help" => array(
+			array("name" => "About", "link" => "about.php", "descr" => "", "authLevelRequired" => 0)
+		)
+	); // end $menu
+
 	
 	echo '<table class="menutable" cellpadding="0" cellspacing="0" border="0">'."\n";
 	
-	for ($i = 0; $i < mysql_num_rows($menu_q); $i++)
+	while (list($menuname, $menuitems) = each($menu))
 	{
-		$menu_row = mysql_fetch_array($menu_q);
-		
-		if ($menu_row["priority"] == 0)
+		// foreach menu item
+		$item_output = "";
+		foreach ($menuitems as $menuitem)
 		{
-			if ($i != 0) {
-				echo "<br />\n";
-				echo "</td></tr>\n";
-			} // end if this is not the first
+			if (get_permit() >= $menuitem["authLevelRequired"])
+			{
+				$item_output .= '<tr><td class="menuitem">'."\n";
+				$item_output .= '<a class="menuitem" href="'. $menuitem["link"] .'" name="'. $menuitem["descr"] .'">';
+				$item_output .= $menuitem["name"];
+				$item_output .= "</a><br />\n";
+				$item_output .= "</td></tr>\n";
+			} // end if we have enough permissions to view this link
+		} // end foreach menu item
 
+		// if we had some item output (ie, we had auth to view at least ONE item in this submenu)
+		if (!empty($item_output))
+		{
+			// output the head
 			echo '<tr><td class="menuhead">'."\n";
-			echo $menu_row["label"];
+			echo $menuname;
 			echo "</td></tr>\n";
+
+			// echo the items
+			echo $item_output;
+
+			// echo the bottom part
 			echo '<tr><td class="menuitem">'."\n";
+			echo "<br />\n";
+			echo "</td></tr>\n";
+		} // end if item output wasn't empty
 
-		} else {
-			echo '<a class="menuitem" href="'. $menu_row["link"] .'" name="'. $menu_row["caption"] .'">';
-			echo $menu_row["label"];
-			echo "</a><br>\n";
+	} // end while we still have menu items
 
-		} // end if it's a head item or not
-	} // end foreach menu item
-
-	echo "</td></tr>\n";
 	echo "</table>\n";
 
 } // end display_menu();
