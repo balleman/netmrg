4a3f9f79a53808aa0e8330bc375d7aba1a59f732
Brady Alleman <brady@alleman.me>
2003-01-16 03:28:01 +0000

    minor cleaning

--- src/netmrg-snmp.cc
+++ src/netmrg-snmp.cc
@@ -36,18 +36,16 @@ string snmpget(DeviceInfo info, string oidstring)
 	strcpy(temp, info.ip.c_str());
 	session.peername = temp;
 
-    /* set up the authentication parameters for talking to the server */
 
-
-    /* set the SNMP version number */
+	/* set the SNMP version number */
 	session.version = SNMP_VERSION_1;
 
-    /* set the SNMPv1 community name used for authentication */
+	/* set the SNMPv1 community name used for authentication */
 	session.community = u_string(info.snmp_read_community, u_temp);
 	session.community_len = info.snmp_read_community.length();
 
 	pthread_mutex_lock(&snmp_lock);
-	sessp = snmp_sess_open(&session);                     /* establish the session */
+	sessp = snmp_sess_open(&session);	/* establish the session */
 	pthread_mutex_unlock(&snmp_lock);
 
 	if (!sessp)
@@ -58,9 +56,9 @@ string snmpget(DeviceInfo info, string oidstring)
 	else 
 	{
 
-    /*
-     * Create the PDU for the data for our request.
-    */
+	/*
+	* Create the PDU for the data for our request.
+	*/
 	pdu = snmp_pdu_create(SNMP_MSG_GET);
 
 	strcpy(tempname, oidstring.c_str());
@@ -73,19 +71,20 @@ string snmpget(DeviceInfo info, string oidstring)
 
 	status = snmp_sess_synch_response(sessp, pdu, &response);
 
-    /*
-     * Process the response.
-     */
+	/*
+	* Process the response.
+	*/
+	
 	if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR)
 	{
-      /*
-       * SUCCESS: Print the result variables
-       */
+		
+	/*
+	* SUCCESS: Print the result variables
+	*/
 
 		if (status == STAT_SUCCESS)
 		{
 			vars = response->variables;
-			//sprint_value(temp, vars->name, vars->name_length, vars);
 			snprint_value(temp, sizeof(temp), vars->name, vars->name_length, vars);
 			result = temp;
 			result = result.erase(0, result.find(":",0) + 1);
