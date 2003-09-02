f3511719114bb70aafd9107bddd6fe3c5adc42cb
Douglas E. Warner <silfreed@silfreed.net>
2003-09-02 23:04:30 +0000

    I broke stuff when I fixed stuff

--- www/lib/database.php
+++ www/lib/database.php
@@ -31,7 +31,14 @@ function db_query($query_string)
 	// if there was an error, handle it
 	if (mysql_errno($GLOBALS["netmrg"]["dbconn"]))
 	{
-
+		if ($GLOBALS["netmrg"]["dbdebug"]) 	 
+		{ 	 
+			die("<b>DB_ERROR:</b> Couldn't execute query:<br>\n<pre>$query_string</pre><br>\n<pre>".mysql_error()."</pre><br>\n\n"); 	 
+		} // end if we're debuging things 	 
+		else 	 
+		{ 	 
+			die("<b>DB_ERROR:</b> Sorry, a database error occured.  We cannot continue.  Please contact the administrator and let them know what you were doing when the problem occured<br><br>\n\n"); 	 
+		} // end else present a nice error code 	 
 	} // end if there was an error
 
 	return $query_result;
