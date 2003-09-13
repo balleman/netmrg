89048da30f769835bea5a53d2def2703d7532517
Douglas E. Warner <silfreed@silfreed.net>
2003-09-13 23:52:11 +0000

    bug#0000050: updater to go from version to version

---
+++ www/webfiles/updater.php
@@ -0,0 +1,125 @@
+<?php
+/********************************************
+* NetMRG Integrator
+*
+* updater.php
+* Updates the current installation
+*
+* see doc/LICENSE for copyright information
+********************************************/
+
+
+require_once("../include/config.php");
+check_auth(3);
+
+// check default action
+if (empty($_REQUEST['action']))
+{
+	$_REQUEST["action"] = "prompt";
+} // end if no action
+
+// database updates that need done
+//    "fromver" => array('query', ..)
+$dbupdates = array(
+	"0.10pre1" => array(
+"ALTER TABLE `graphs` ADD `options` SET( 'nolegend', 'logarithmic' ) NOT NULL ;",
+"ALTER TABLE `graphs` ADD `base` INT DEFAULT '1000' NOT NULL ;",
+"ALTER TABLE `graph_ds` ADD `start_time` VARCHAR( 20 ) NOT NULL ;",
+"ALTER TABLE `graph_ds` ADD `end_time` VARCHAR( 20 ) NOT NULL ;",
+"ALTER TABLE `graphs` ADD `title` VARCHAR( 100 ) NOT NULL AFTER `name`;",
+"ALTER TABLE `graph_ds` CHANGE `multiplier` `multiplier` VARCHAR( 100 ) DEFAULT '1' NOT NULL;",
+"ALTER TABLE snmp_interface_cache ADD COLUMN ifSpeed int(11) NOT NULL;"
+	) // end 0.10pre1
+); // end $dbupdates;
+
+// check what to do
+switch ($_REQUEST['action'])
+{
+	case "update":
+		update($dbupdates);
+		break;
+		
+	case "prompt":
+	default:
+		prompt();
+		break;
+} // end switch action
+
+
+
+/***** FUNCTIONS *****/
+
+/**
+* prompt()
+*
+* tells a user what we're about to do
+*/
+function prompt()
+{
+	begin_page("updater.php", "Updater");
+	
+	$dbver = GetDBVersion();
+	
+	if ($dbver != $GLOBALS["netmrg"]["version"])
+	{
+?>
+The current database needs to be updated from version 
+<b><?php echo $dbver; ?></b> to <b><?php echo $GLOBALS["netmrg"]["version"] ?></b><br />
+<br />
+<?php
+		make_edit_table("Update Database?");
+		make_edit_hidden("action", "update");
+		make_edit_submit_button();
+		make_edit_end();
+	} // end if database needs updated
+	else
+	{
+?>
+Your database is already at the latest version, 
+<b><?php echo $dbver; ?></b>.  No upgrade is needed.
+<?php
+	} // end if no changes
+	end_page();
+} // end edit();
+
+
+/**
+* update($dbupdates)
+*
+* update's an installation
+*/
+function update($dbupdates)
+{
+	begin_page("updater.php", "Updater");
+	
+	$dbver = GetDBVersion();
+	$doupdates = false;
+	
+	while (list($dbupver, $dbqueries) = each($dbupdates))
+	{
+		if ($dbupver == $dbver)
+		{
+			$doupdates = true;
+		} // end if we've reached the version to do updates for
+		else if ($dbupver == $GLOBALS["netmrg"]["version"])
+		{
+			break;
+		} // end if we're at the current version
+		
+		if ($doupdates)
+		{
+			echo "<b>$dbupver</b><br />\n";
+			foreach($dbqueries as $dbquery)
+			{
+				echo " .";
+				db_query($dbquery);
+			} // end foreach query in this version
+			echo "<br /><br />\n";
+		} // end if we're to do the updates
+	} // end foreach version
+	
+	end_page();
+} // end update();
+
+
+?>
