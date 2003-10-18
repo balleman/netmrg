24b98051be79fe5b880404a4cc251d9e18049769
Douglas E. Warner <silfreed@silfreed.net>
2003-10-18 17:57:01 +0000

    just a quick little perl script to capture apm info on laptops and report it in a netmrg-friendly format

---
+++ libexec/linuxapminfo.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+#
+# linuxapminfo.pl
+#
+# reports values of certain apm values
+#
+
+use strict;
+
+if (scalar(@ARGV) != 1 
+	|| ($ARGV[0] ne "-p" && $ARGV[0] ne "-t")
+	)
+{
+	print "U\n";
+	print "\n";
+	print "$0 [-p] [-t]\n";
+	print "  -p: Report Percent Left\n";
+	print "  -t: Report Time Remaining (in minutes)\n";
+	print "\n";
+	exit(1);
+} # end if not enough parameters
+
+# read in info from apm
+open 'apmfh', "/proc/apm";
+my $apmline = <apmfh>;
+close 'apmfh';
+
+## get the value we want
+# percent left
+if ($ARGV[0] eq "-p")
+{
+	if ($apmline =~ /(\d+)%/)
+	{
+		print "$1\n";
+		exit(0);
+	} # end if found percent left
+} # end if percent left to check
+
+# time remaining
+elsif ($ARGV[0] eq "-t")
+{
+	if ($apmline =~ /(\d+)\s+min$/)
+	{
+		print "$1\n";
+		exit(0);
+	} # end if found min left
+} # end if time remaining
+
+print "U\n";
+exit (1);
+
