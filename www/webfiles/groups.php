e6444b05dd92a5ace89f9457ae4110c31477578e
Douglas E. Warner <silfreed@silfreed.net>
2003-12-30 23:54:43 +0000

    bug#0000124 - cleanup groups.php

--- www/webfiles/groups.php
+++ www/webfiles/groups.php
@@ -12,51 +12,112 @@
 require_once("../include/config.php");
 check_auth(1);
 
-if (!isset($_REQUEST["action"]) || ($_REQUEST["action"] == "doedit" || $_REQUEST["action"] == "dodelete" || $_REQUEST["action"] == "doadd"))
+// if no action, set a default one
+if (empty($_REQUEST["action"]))
 {
-// Change databases if necessary and then display list
+	$_REQUEST["action"] = "list";
+} // end if no action
 
-	if (!empty($_REQUEST["action"]) && ($_REQUEST["action"] == "doedit" || $_REQUEST["action"] == "doadd"))
-	{
+
+// what to do
+switch ($_REQUEST["action"])
+{
+	case "edit" :
 		check_auth(2);
-		if ($_REQUEST["action"] == "doedit")
-		{
-			if ($_REQUEST["grp_id"] == -1)
-			{
-				create_group($_REQUEST["grp_name"], $_REQUEST["grp_comment"], $_REQUEST["parent_id"]);
-			}
-			else
-			{
-				update_group($_REQUEST["grp_id"], $_REQUEST["grp_name"], $_REQUEST["grp_comment"], $_REQUEST["parent_id"]);
-			} // end if group id
-		} // end if action is to edit
-	} // done editing
-
-
-	if (!empty($_REQUEST["action"]) && $_REQUEST["action"] == "dodelete")
-	{
+		edit();
+		break;
+		
+	case "add"  :
+		check_auth(2);
+		add();
+		break;
+		
+	case "update" :
+		check_auth(2);
+		update_group($_REQUEST["grp_id"], $_REQUEST["grp_name"], $_REQUEST["grp_comment"], $_REQUEST["parent_id"]);
+		display();
+		break;
+		
+	case "insert" :
+		check_auth(2);
+		create_group($_REQUEST["grp_name"], $_REQUEST["grp_comment"], $_REQUEST["parent_id"]);
+		display();
+		break;
+		
+	case "delete" :
 		check_auth(2);
 		delete_group($_REQUEST["grp_id"]);
-	} // done deleting
+		display();
+		break;
+		
+	default :
+	case "list" :
+		display();
+		break;
+} // end what to do
 
 
+
+/***** FUNCTIONS *****/
+function edit()
+{
+	// Display editing screen
+	begin_page("groups.php", "Groups");
+	
+	$grp_id = $_REQUEST["grp_id"];
+	$grp_results = db_query("SELECT * FROM groups WHERE id=$grp_id");
+	$grp_row = db_fetch_array($grp_results);
+	$grp_name = $grp_row["name"];
+	$grp_comment = $grp_row["comment"];
+	
+	make_edit_table("Edit Group");
+	make_edit_text("Name:","grp_name","25","100",$grp_name);
+	make_edit_text("Comment:","grp_comment","50","200",$grp_comment);
+	make_edit_select_from_table("Parent:", "parent_id", "groups", $grp_row["parent_id"], "", array(0 => "-Root-"), array(), "id != '$grp_id'");
+	make_edit_hidden("grp_id", $grp_id);
+	make_edit_hidden("action","update");
+	make_edit_submit_button();
+	make_edit_end();
+	end_page();
+} // end edit();
+
+
+function add()
+{
+	// Display editing screen
+	begin_page("groups.php", "Groups");
+	
+	make_edit_table("Edit Group");
+	make_edit_text("Name:","grp_name","25","100","");
+	make_edit_text("Comment:","grp_comment","50","200","");
+	make_edit_select_from_table("Parent:", "parent_id", "groups", 0, "", array(0 => "-Root-"), array(), "id != '-1'");
+	make_edit_hidden("grp_id", -1);
+	make_edit_hidden("action","insert");
+	make_edit_submit_button();
+	make_edit_end();
+	end_page();
+} // end add();
+
+
+function display()
+{
 	// Display a list
 	begin_page("groups.php", "Groups");
-	js_confirm_dialog("del", "Are you sure you want to delete group ", " and all associated items?", "{$_SERVER['PHP_SELF']}?action=dodelete&grp_id=");
-	make_display_table("Device Groups", "", 
+	js_confirm_dialog("del", "Are you sure you want to delete group ", " and all associated items?", "{$_SERVER['PHP_SELF']}?action=delete&grp_id=");
+	make_display_table("Device Groups", "",
 		array("text" => "Name"),
 		array("text" => "Comment")
 	); // end make_display_table();
-
+	
 	if (!isset($_REQUEST["orderby"]))
-	{ 
+	{
 		$orderby = "name";
 	}
 	else
 	{
 		$orderby = $_REQUEST["orderby"];
 	} // end if orderby
-
+	
 	if (!isset($_REQUEST["parent_id"]))
 	{
 		$parent_id = 0;
@@ -65,11 +126,11 @@ if (!isset($_REQUEST["action"]) || ($_REQUEST["action"] == "doedit" || $_REQUEST
 	{
 		$parent_id = $_REQUEST["parent_id"];
 	} // end if parent id
-
+	
 	$grp_select = "SELECT * FROM groups WHERE parent_id=$parent_id ORDER BY $orderby";
 	$grp_results = db_query($grp_select);
 	$grp_total = db_num_rows($grp_results);
-
+	
 	// For each group
 	for ($grp_count = 1; $grp_count <= $grp_total; ++$grp_count)
 	{
@@ -94,44 +155,12 @@ if (!isset($_REQUEST["action"]) || ($_REQUEST["action"] == "doedit" || $_REQUEST
 				formatted_link("Delete", "javascript:del('" . addslashes($grp_row["name"]) . "', '" . $grp_row["id"] . "')"))
 		); // end make_display_item();
 	} // end foreach group
-
+	
 ?>
 </table>
 <?php
-} // End if no action
+	end_page();
+} // end list();
 
-if (!empty($_REQUEST["action"]) && ($_REQUEST["action"] == "edit" || $_REQUEST["action"] == "add"))
-{
-	// Display editing screen
-	check_auth(2);
-	begin_page("groups.php", "Groups");
-	if ($_REQUEST["action"] == "add")
-	{
-		$grp_id = -1;
-	}
-	else
-	{
-		$grp_id = $_REQUEST["grp_id"];
-	} // end if this is an add, no parent group
-
-	$grp_results = db_query("SELECT * FROM groups WHERE id=$grp_id");
-	$grp_row = db_fetch_array($grp_results);
-	$grp_name = $grp_row["name"];
-	$grp_comment = $grp_row["comment"];
-
-	if ($_REQUEST["action"] == "add") { $grp_row["parent_id"] = 0; }
-
-	make_edit_table("Edit Group");
-	make_edit_text("Name:","grp_name","25","100",$grp_name);
-	make_edit_text("Comment:","grp_comment","50","200",$grp_comment);
-	make_edit_select_from_table("Parent:", "parent_id", "groups", $grp_row["parent_id"], "", array(0 => "-Root-"), array(), "id != '$grp_id'");
-	make_edit_hidden("grp_id", $grp_id);
-	make_edit_hidden("action","doedit");
-	make_edit_submit_button();
-	make_edit_end();
-
-} // End editing screen
-
-end_page();
 
 ?>
