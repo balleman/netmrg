0476c97844ea82f309888b95d8dfaf06d8bc5a82
Kevin Bonner <keb@nivek.ws>
2004-11-17 01:02:44 +0000

    bug#0000345 - get_graph.php needs cleaned up

--- www/webfiles/get_graph.php
+++ www/webfiles/get_graph.php
@@ -16,12 +16,10 @@ if (isset($_REQUEST["debug"]))
 	include("../lib/phptimer.php");
 	$timer = new PHP_timer;
 	$timer->start();
+	$timer->addmarker("before headers");
 } // end if debug
 
 /***** Expiration Headers *****/
-
-// debug
-if (isset($_REQUEST["debug"])) { $timer->addmarker("before headers"); }
 // Date in the past
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 // Always modified
@@ -32,11 +30,16 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 // HTTP/1.0
 header("Pragma: no-cache");
 
-// Image Type
-if (!isset($_REQUEST['debug'])) header("Content-type: image/png");
+if (isset($_REQUEST["debug"]))
+{
+	$timer->addmarker("before auth");
+}
+else
+{
+	// Image Type
+	header("Content-type: image/png");
+}
 
-// debug
-if (isset($_REQUEST["debug"])) { $timer->addmarker("before auth"); }
 // we need to auth different ways depending on type of graph
 switch($_REQUEST["type"])
 {
@@ -49,7 +52,7 @@ switch($_REQUEST["type"])
 		break;
 		
 	case "custom_item" :
-		$q = db_query("SELECT graph_id FROM graph_ds WHERE id={$_REQUEST['id']}");
+		$q = db_query("SELECT graph_id FROM graph_ds WHERE id='{$_REQUEST['id']}'");
 		$r = db_fetch_array($q);
 		GraphCheckAuth($_REQUEST["type"], $r['graph_id']);
 		break;
@@ -58,39 +61,36 @@ switch($_REQUEST["type"])
 		GraphCheckAuth($_REQUEST["type"], $_REQUEST["id"]);
 		break;
 } // end switch type for auth
-// debug
-if (isset($_REQUEST["debug"])) { $timer->addmarker("after auth"); }
 
 if (empty($_REQUEST["hist"]))
 {
 	$_REQUEST["hist"] = 0;
 }
 
-// debug
-if (isset($_REQUEST["debug"])) { $timer->addmarker("before get command"); }
+if (isset($_REQUEST["debug"]))
+{
+	$timer->addmarker("after auth");
+	$timer->addmarker("before get command");
+}
+
 // figure out what our command for generating the graph will be
 $command = get_graph_command($_REQUEST["type"], $_REQUEST["id"], $_REQUEST["hist"], $_REQUEST["type"] == "template");
-// debug
-if (isset($_REQUEST["debug"])) { $timer->addmarker("after get command"); }
 
-if (isset($_REQUEST["debug"])) { $timer->addmarker("before exec command"); }
-if (isset($_REQUEST['debug']))
+if (isset($_REQUEST["debug"]))
 {
+	$timer->addmarker("after get command");
+	$timer->addmarker("before exec command");
 	echo(rrdtool_syntax_highlight($command));
 	exec($command);
+	$timer->addmarker("after exec command");
+	echo("<br><br>");
+	$timer->stop();
+	$timer->debug();
+	$timer->showtime();
 }
 else
 {
 	passthru($command);
 }
-if (isset($_REQUEST["debug"])) { $timer->addmarker("after exec command"); }
-
-if (isset($_REQUEST["debug"]))
-{
-	echo("<br><br>");
-	$timer->stop();
-	$timer->debug();
-	$timer->showtime();
-} // end if debug
 
 ?>
