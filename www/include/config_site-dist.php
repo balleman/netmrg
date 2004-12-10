a72537b05d832061866c9a44e85541733a480d65
Douglas E. Warner <silfreed@silfreed.net>
2004-12-10 00:59:09 +0000

    bug#0000281 - Support for a config_site.php in addition to config.php

---
+++ www/include/config_site-dist.php
@@ -0,0 +1,25 @@
+<?php
+/********************************************
+* NetMRG Integrator
+* 
+* config_site.php
+* Local Site Configuration
+*
+* see doc/LICENSE for copyright information
+********************************************/
+
+// Error Reporting Level
+// error_reporting(E_ALL ^ E_NOTICE);
+
+// DB Config
+// $GLOBALS["netmrg"]["dbdebug"]           = true;
+
+// Authentication Config
+// $GLOBALS["netmrg"]["authTimeout"]       = 86400; // 24hr * 60min * 60sec
+// $GLOBALS["netmrg"]["defaultMapUser"]    = "NETMRG_DEFAULT_MAP";
+
+// Template Config
+// $GLOBALS["netmrg"]["traffictemplateid"] = 7;
+// $GLOBALS["netmrg"]["disktemplateid"]	= 8;
+
+?>
