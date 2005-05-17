fd49eda4c155b0826811097daec4c9c7d8165f5e
Douglas E. Warner <silfreed@silfreed.net>
2005-05-17 00:51:20 +0000

    bug#0000365: fixing javascript to collapse all items before opening a new menu

--- www/webfiles/include/navhide.js
+++ www/webfiles/include/navhide.js
@@ -2,6 +2,12 @@
 // all content within span tags
 function NavHide(MenuItem)
 {
+	var MenuItems = new Array('topmenu_Reporting', 'topmenu_Admin', 'topmenu_Prefs', 'topmenu_Logout', 'topmenu_Help');
+	for (var i = 0; i < MenuItems.length; i++)
+	{
+		document.getElementById(MenuItems[i]).style.display = "none";
+	} // end foreach menu item
+	
 	if (document.getElementById(MenuItem).style.display == "none")
 	{
 		document.getElementById(MenuItem).style.display = "";
