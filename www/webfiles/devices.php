570f226ee37a463482ad9237b35fcd68583c529e
Douglas E. Warner <silfreed@silfreed.net>
2008-07-08 13:51:20 +0000

    fixing license; removing references to MIT adding license info to all files

--- www/webfiles/devices.php
+++ www/webfiles/devices.php
@@ -5,7 +5,25 @@
 * devices.php
 * Monitored Devices Editing Page
 *
-* see doc/LICENSE for copyright information
+* Copyright (C) 2001-2008
+*   Brady Alleman <brady@thtech.net>
+*   Douglas E. Warner <silfreed@silfreed.net>
+*   Kevin Bonner <keb@nivek.ws>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
 ********************************************/
 
 
